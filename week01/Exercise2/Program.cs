7578cb6d38e06c97176e4a41a1f36d759d7f9283
jdethington <jdethington@gmail.com>
2025-12-26 16:55:52 -0700

    Finished Core requirements

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -5,5 +5,44 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
+
+        Console.Write("What is your grade percentage? ");
+        string gradePercentString = Console.ReadLine();
+        float gradePercent = float.Parse(gradePercentString);
+
+        string letter;
+        string words;
+
+        if (gradePercent >= 70)
+        {
+            words = "Congradulations! You passed the class.";
+
+            if (gradePercent >= 90)
+            {
+                letter = "A";
+            }
+            else if (gradePercent >= 80)
+            {
+                letter = "B";
+            }
+            else
+            {
+                letter = "C";
+            }
+        }
+        else
+        {
+            words = "Try again next time.";
+            if (gradePercent >= 60)
+            {
+                letter = "D";
+            }
+            else
+            {
+                letter = "F";
+            }
+        }
+
+        Console.WriteLine($"You got a grade of {letter}. {words}");
     }
 }
